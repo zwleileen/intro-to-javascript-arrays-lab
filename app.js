d3b598a3bd2f3b26c1c9bb582f02bdf1e3f680cb
eileen <zwleileen@gmail.com>
2024-12-10 14:15:30 +0800

    feat:completed exercises 1 and 2

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+// console.log("Hello World")
+
+// Exercise 1
+const foods = [];
+console.log("Exercise 1 result", foods);
+
+// Exercise 2
+foods.push("pizza", "cheeseburger");
+console.log("Exercise 2 result:", foods);
