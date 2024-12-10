e809c70b86b1d12c92d900d2426f708d6e3c516c
eileen <zwleileen@gmail.com>
2024-12-10 14:39:01 +0800

    feat:completed exercises 3 and 4

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ console.log("Exercise 1 result", foods);
 // Exercise 2
 foods.push("pizza", "cheeseburger");
 console.log("Exercise 2 result:", foods);
+
+// Exercise 3
+foods.unshift("taco");
+console.log("Exercise 3 result:", foods);
+
+// Exercise 4
+const favFood = foods[1];
+console.log("Exercise 4 result:", favFood);
